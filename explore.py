f186dd8442c77f35b78df11ec1a7b04a678a02db
Evgeniy Mamchenko <evgeniy.mamchenko@yandex.ru>
2019-05-19 17:12:50 +0200

    move extraction code to a separate file, extract features for every photo, cache results in a file

--- explore.py
+++ explore.py
@@ -14,8 +14,8 @@
 #     name: python3
 # ---
 
-# # What information we can get from photos of businesses?
-#
+# # What information can we get from photos of businesses?
+#  
 # Working with raw image data is infeasible, not only because its total approximate size is `200k*400*400*3 ≈ 100GB`.
 #
 # Also, direct pixel values are not very useful as features as photos of same objects can be quite far away in RGB space.
@@ -25,37 +25,11 @@
 # Following [CNN features are also great at unsupervised classification](https://arxiv.org/abs/1707.01700) that demonstrates that combination of Xception architecture and Agglomerative Hierarchical Clustering gives the best clustering results (using Normalized Mutual Information score) let us start with these.
 #
 # The implementation of Xception trained on ImageNet is available at https://github.com/Cadene/pretrained-models.pytorch.
+#
+# We need to get features after the last pooling layer so `xception_with_pooling_features.py` was writen to achive this.
 
-from xception_with_pooling_features import xception_with_pooling_features
-xception_model = xception_with_pooling_features(num_classes=1000, pretrained='imagenet')
+import feature_extractor
 
-# +
-import torch
-import pretrainedmodels.utils as utils
-
-load_img = utils.LoadImage()
-
-# transformations depending on the model
-# rescale, center crop, normalize, and others (ex: ToBGR, ToRange255)
-tf_img = utils.TransformImage(xception_model) 
-
-#path_img = 'photos/photos/3-T7FTL_sB4mH2bc3WnPUg.jpg'
-path_img = 'photos/photos/3T449kzPrsipjBx7I8ENag.jpg'
-
-input_img = load_img(path_img)
-input_tensor = tf_img(input_img)         # 3x400x225 -> 3x299x299 size may differ
-input_tensor = input_tensor.unsqueeze(0) # 3x299x299 -> 1x3x299x299
-input = torch.autograd.Variable(input_tensor,
-    requires_grad=False)
-
-#output_logits = model(input) # 1x1000
-
-output_features = xception_model.features(input) # 1x14x14x2048 size may differ
-# -
-
-output_features.shape
-
-output_features
-
+feature_extractor.load_photos_with_features_df().head()
 
 
---
+++ feature_extractor.py
@@ -0,0 +1,56 @@
+"""
+Extract features from all photos and caches the result in a file.
+"""
+import os
+
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from xception_with_pooling_features import xception_with_pooling_features
+import pretrainedmodels.utils as utils
+
+def extract():
+    cuda_is_availabe = torch.cuda.is_available()
+    print('CUDA is available' if cuda_is_availabe else 'CUDA is NOT available')
+    xception_model = xception_with_pooling_features(num_classes=1000, pretrained='imagenet')
+    if cuda_is_availabe:
+        xception_model = xception_model.to(torch.device('cuda:0'))
+    photo_df = pd.read_json('photos/photo.json', lines=True)
+
+    load_img = utils.LoadImage()
+
+    # transformations depending on the model
+    # rescale, center crop, normalize, and others (ex: ToBGR, ToRange255)
+    tf_img = utils.TransformImage(xception_model)
+
+    photo_df['features'] = None
+    # Extract features just for a small subset.
+    #photo_df = photo_df.loc[:100-1,:]
+    for index, row in tqdm(photo_df.iterrows(), total=photo_df.shape[0], desc="Extracting features from photos"):
+        photo_id = row['photo_id']
+        file_name = os.path.join('photos', 'photos', photo_id + '.jpg')
+        input_img = load_img(file_name)
+        input_tensor = tf_img(input_img)  # 3x?x? -> 3x299x299 size may differ
+        input_tensor = input_tensor.unsqueeze(0)  # 3x299x299 -> 1x3x299x299
+        if cuda_is_availabe:
+            input_tensor = input_tensor.cuda()
+        with torch.no_grad():
+            #input = torch.autograd.Variable(input_tensor, requires_grad=False)
+            output_features = xception_model.features(input_tensor).cpu().numpy()  # 1x2048x1x1
+        output_features = np.reshape(output_features, (-1,))
+        row['features'] = output_features
+
+    os.makedirs('.cache', exist_ok=True)
+    photo_df.to_pickle('.cache/photo.pkl')
+
+
+def load_photos_with_features_df():
+    if not os.path.isfile('.cache/photo.pkl'):
+        extract()
+    return pd.read_pickle('.cache/photo.pkl')
+
+
+if __name__== "__main__":
+  extract()
